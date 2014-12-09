6230d943a1e3f072a5e50450e33e631ed1550185
Mackan90096 <afmin@mackanzweebb.comze.com>
2014-12-09 10:48:30 +0100

    Update item.lua

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -1,6 +1,5 @@
 data:extend({
-
- {
+  {
     type = "item",
     name = "compressed-coal",
     icon = "__Factorio_Mod__/graphics/icon_compresscoal.png",
@@ -8,5 +7,14 @@ data:extend({
     subgroup = "energy",
     stack_size = 200,
     fuel_value = 50
+  },
+  {
+    type = "item",
+    name = "giant-chest",
+    icon = "__base__/graphics/icons/wooden-chest.png",
+    flags = { "goes-to-quickbar" },
+    subgroup = "logistics",
+    stack_size = 50,
+    fuel_value = 0.1
   }
 })
